56207cc8944752b2788db2a6f914e8c1095c8e39
Meriyan <it21076916@my.sliit.lk>
2023-07-02 07:54:20 +0530

    dark mode

--- js/script.js
+++ js/script.js
@@ -33,5 +33,14 @@ let header = document.querySelector('.header');
 header.classList.toggle('sticky',window.scrollY > 100);
 
 // remove menu icon navbar when click navbar link (scroll)
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+};
 
+// dark light mode
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
 };
